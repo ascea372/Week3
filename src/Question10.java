3e6a4d910263a03c0ea2342610b915607059bdc9
ascea372 <wi526124@gmail.com>
2024-10-02 14:23:42 +0900

    처음 커밋

--- src/Question10.java
+++ src/Question10.java
@@ -9,13 +9,13 @@ public class Question10 {
         if (num % 3 == 0 && num % 5 == 0) {
             System.out.println("3과 5의 배수입니다.");
         }
-        else if (num % 3 == 0 && num % 5 != 0) {
+        else if (num % 3 == 0) {
             System.out.println("3의 배수입니다.");
         }
-        else if (num % 5 == 0 && num % 3 != 0) {
+        else if (num % 5 == 0) {
             System.out.println("5의 배수입니다.");
         }
-        else if (num % 3 != 0 && num % 5 != 0) {
+        else {
             System.out.println("해당 조건을 만족하지 않습니다.");
         }
     }
